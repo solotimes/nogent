744ec51a1b6db6ef182a0563de2b55ecfec226d4
solotimes <toknight@gmail.com>
2013-02-07 11:22:07 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nogent::Application.config.secret_token = '54d8026f72a0ac8be6f102f418944118f553994ebd6851fa6890de85b685820444d30c16bfdc0e995a0d864e52198f3fe19f6120d68cbeb89c152924ef6325d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nogent::Application.config.session_store :cookie_store, key: '_nogent_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nogent::Application.config.session_store :active_record_store
