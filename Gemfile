02d4b4ddf63a3735fd31b5b1d26287ff60968459
solotimes <toknight@gmail.com>
2013-02-11 15:38:56 +0800

    整理gem和配置

--- Gemfile
+++ Gemfile
@@ -4,19 +4,23 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'anjlab-bootstrap-rails', '~> 2.3', :require => 'bootstrap-rails'
 end
 gem 'jquery-rails'
 gem "mongoid", ">= 3.0.19"
 gem "haml", ">= 3.1.7"
 gem "haml-rails", ">= 0.3.5", :group => :development
-gem "hpricot", ">= 0.8.6", :group => :development
-gem "ruby_parser", ">= 3.1.1", :group => :development
 gem "rspec-rails", ">= 2.12.2", :group => [:development, :test]
+
 gem "database_cleaner", ">= 0.9.1", :group => :test
 gem "mongoid-rspec", ">= 1.6.0", :group => :test
 gem "email_spec", ">= 1.4.0", :group => :test
+
 gem "factory_girl_rails", ">= 4.2.0", :group => [:development, :test]
 gem "quiet_assets", ">= 1.0.1", :group => :development
 gem "better_errors", ">= 0.3.2", :group => :development
 gem "binding_of_caller", ">= 0.6.8", :group => :development
-gem "xtunes_helpers", :git => "git@github.com:xtunes/xtunes_helpers.git"
+gem "inherited_resources"
+gem "rails_config"
+gem 'kaminari'
+gem 'simple_form'
--- config/application.rb
+++ config/application.rb
@@ -20,19 +20,14 @@ module Nogent
     config.time_zone = 'Beijing'
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**' ,'*.{rb,yml}').to_s]
     config.i18n.default_locale = "zh-CN"
-    Time::DATE_FORMATS.merge!(:default => "%Y/%m/%d %H:%M:%S", :ymd => "%Y/%m/%d")
-
-
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
-      
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
-      
-      
       g.view_specs false
       g.helper_specs false
+      g.helper false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
@@ -43,21 +38,8 @@ module Nogent
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -68,21 +50,14 @@ module Nogent
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
-
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    # config.active_record.whitelist_attributes = true
-
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
+end
+
+RailsConfig.setup do |config|
+  config.const_name = "Settings"
 end
